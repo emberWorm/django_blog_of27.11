7870457662e670d50c2e717cf60a47c1fe2cac3f
emberWorm <wrie.cope@gmail.com>
2025-01-11 18:25:36 +0300

    add new url & template

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', home)
+    path('', home),
+    path('new_temp/', new_temp, name='new_temp'),
 ]
--- app/views.py
+++ app/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 def home(request):
     posts = Post.objects.all()
     return render(request, "home.html", {"posts": posts})
+
+
+def new_temp(request):
+    return render(request, "new_template.html")
